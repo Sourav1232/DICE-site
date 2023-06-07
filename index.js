c63f4bf9d1e27ac5f6b62dd80486b031296ac522
Sourav1232 <92489450+Sourav1232@users.noreply.github.com>
2023-06-07 21:06:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 randomNumber1=Math.random()*6;
 randomNumber1=Math.floor(randomNumber1)+1;
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img1").setAttribute("src","dice"+randomNumber1+".png");
 randomNumber2=Math.random()*6;
 randomNumber2=Math.floor(randomNumber2)+1;
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+document.querySelector(".img2").setAttribute("src","dice"+randomNumber2+".png");
 if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="Player1 Wins!🚩";
 }
